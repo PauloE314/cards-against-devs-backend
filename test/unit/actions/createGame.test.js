b9cb80f26aeaac1c4da2773223b3926c0a68f114
Paulo <pauloeduardodelima155@gmail.com>
2022-06-27 16:45:08 -0300

    Removes player repository

--- test/unit/actions/createGame.test.js
+++ test/unit/actions/createGame.test.js
@@ -1,10 +1,11 @@
 const { createGame } = require('../../../src/game/actions');
-const { GamesRepository } = require('../../../src/game/repository/GamesRepository');
+const {
+  GamesRepository,
+} = require('../../../src/game/repository/GamesRepository');
 const { Game } = require('../../../src/game/entities/Game');
 const { Player } = require('../../../src/game/entities/Player');
 
 jest.mock('../../../src/game/repository/GamesRepository');
-jest.mock('../../../src/game/repository/PlayersRepository');
 
 describe('createGame', () => {
   const socketId = '123';
